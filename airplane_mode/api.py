96cf5b78644527d6985e21cf0c7be0810e84f23b
AksharPrajapati <aksharpp369@gmail.com>
2024-07-12 14:39:34 +0530

    test: add webhook verification

--- airplane_mode/api.py
+++ airplane_mode/api.py
@@ -1,29 +1,58 @@
-# airplane_mode/api.py
-
-import os
 import frappe # type: ignore
+import json
 import requests
+from werkzeug.wrappers import Response # type: ignore
 from frappe import _ # type: ignore
+from hashlib import sha1
+import hmac
 
 @frappe.whitelist(allow_guest=True)
 def handle_facebook_webhook():
     print("Entry....")
-    try:
-        data = frappe.request.json
-        if "entry" in data:
-            for entry in data["entry"]:
-                if "changes" in entry:
-                    for change in entry["changes"]:
-                        if change["field"] == "leadgen":
-                            leadgen_id = change["value"]["leadgen_id"]
-                            access_token = "your-page-access-token"
-                            fetch_lead_data(leadgen_id, access_token)
-        return {"status": "success"}
-    except Exception as e:
-        frappe.log_error(frappe.get_traceback(), _("Facebook Webhook Error"))
-        return {"status": "error", "message": str(e)}
+    if frappe.request.method == "GET":
+        if (frappe.request.args.get("hub.mode") == "subscribe" and
+            frappe.request.args.get("hub.verify_token") == frappe.conf.facebook_verify_token):
+            return frappe.request.args.get("hub.challenge")
+        else:
+            return "Verification token mismatch", 403
 
-def fetch_lead_data(leadgen_id, access_token):
+    elif frappe.request.method == "POST":
+        calculated_signature = calculate_signature(frappe.request.get_data())
+        print("Calculated Signature: sha1=" + calculated_signature)
+
+        if not verify_signature(frappe.request, "sha1=" + calculated_signature):
+            return "Invalid signature", 401
+
+        data = frappe.request.json
+        frappe.logger().info("Facebook request body: {}".format(json.dumps(data)))
+        process_facebook_updates(data)
+        return {"status": "success"}
+
+def calculate_signature(payload):
+    app_secret = frappe.conf.facebook_app_secret
+    mac = hmac.new(bytes(app_secret, 'utf-8'), msg=payload, digestmod=sha1)
+    return mac.hexdigest()
+
+def verify_signature(request, calculated_signature):
+    signature = calculated_signature
+    print(signature)
+    if not signature:
+        return False
+
+    sha_name, signature = signature.split('=')
+    mac = hmac.new(bytes(frappe.conf.facebook_app_secret, 'utf-8'), msg=request.get_data(), digestmod=sha1)
+    return hmac.compare_digest(mac.hexdigest(), signature)
+
+def process_facebook_updates(data):
+    if "entry" in data:
+        for entry in data["entry"]:
+            if "changes" in entry:
+                for change in entry["changes"]:
+                    if change["field"] == "leadgen":
+                        leadgen_id = change["value"]["leadgen_id"]
+                        fetch_lead_data(leadgen_id)
+
+def fetch_lead_data(leadgen_id):
     conf = frappe.get_conf()
     print("FETCH LEAD DATA....")
     url = f"https://graph.facebook.com/v11.0/{leadgen_id}"
@@ -37,6 +66,7 @@ def fetch_lead_data(leadgen_id, access_token):
 def process_lead_data(lead_data, api_key, api_secret):
     field_data = lead_data.get("field_data", [])
     lead_info = {field["name"]: field["values"][0] for field in field_data}
+    print(lead_info)
     
     # Format phone number if it contains a country code
     phone_number = lead_info.get("phone_number")
